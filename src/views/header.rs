817420441f42c0f97382a4a38f68d11ec46f61c0
Fgdou <fabigoardou@gmail.com>
2026-04-14 21:29:23 +0200

    feat bootstrap

--- src/views/header.rs
+++ src/views/header.rs
@@ -4,16 +4,20 @@ pub fn template(page: Markup) -> Markup {
     html!(
         head {
             title { "MusiSeerr" }
-            script src="/static/htmx.min.js" {}
+            link href="/static/bootstrap.min.css" rel="stylesheet" {}
+            meta name="viewport" content="width=device-width, initial-scale=1" {}
         }
         body {
             h1 {
                 "MusiSeerr"
             }
 
-            content {
+            div class="container" {
                 (page)
             }
+
+            script src="/static/htmx.min.js" {}
+            script src="/static/bootstrap.min.js" {}
         }
     )
 }
--- src/views/search.rs
+++ src/views/search.rs
@@ -4,9 +4,11 @@ use crate::objects::{SearchParameters, SearchResult};
 
 pub fn search_bar(parameters: &SearchParameters) -> Markup {
     html!(
-        form hx-get="./search" hx-target="#search-content" hx-disabled-elt="#search-button" hx-replace-url="true" {
-            input name="query" value=(parameters.query.as_ref().unwrap_or(&"".to_string())) placeholder="Search text" {}
-            button type="submit" id="search-button" { "search" }
+        form class="search-bar text-center row g-2" hx-get="./search" hx-target="#search-content" hx-disabled-elt="#search-button" hx-replace-url="true" {
+            div class="input-group" {
+                input class="form-control" name="query" value=(parameters.query.as_ref().unwrap_or(&"".to_string())) placeholder="Search text" {}
+                button type="submit" id="search-button" class="btn btn-primary" { "search" }
+            }
         }
     )
 }
@@ -15,14 +17,12 @@ pub fn search_result(result: &SearchResult) -> Markup {
     html!{
         h1 { "Musics" }
 
-        table {
-            thead {
+        table class="table align-middle" {
+            thead class="sticky-top" {
                 tr {
                     th {"Title"}
                     th {"Album"}
-                    th {"Type"}
                     th {"Artist"}
-                    th {"Monitored"}
                     th {"Request"}
                 }
             }
@@ -31,12 +31,10 @@ pub fn search_result(result: &SearchResult) -> Markup {
                     tr { 
                         td {(music.title)}
                         td {(music.album)}
-                        td {(music.album_type)}
                         td {(music.artist)}
-                        td {(music.monitored)}
                         td {
                             form hx-post="/request_music" hx-disabled-elt="find button" hx-target="this" hx-swap="outerHTML" {
-                                button type="submit" disabled[music.monitored] {
+                                button class="btn btn-secondary" type="submit" disabled[music.monitored] {
                                     "Request"
                                 }
                                 input type="hidden" value=(music.id) name="music_id" {}
